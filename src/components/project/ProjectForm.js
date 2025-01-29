acbea0ef4d947177783aa09c5014b49d91e453c7
João Eduardo Rocha Alves <joao.eduardo0418@hotmail.com>
2025-01-29 14:29:42 -0300

    opçoes do select
    foi feito as opções do select utilizando um falso servidor

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import {useEffect, useState} from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import Submit from '../form/Submit'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+const [categories, setCategories] = useState([])
+
+useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch((err) => console.log(err))
+}, [])
+
     return (
         <form className={styles.form}>
 
@@ -21,7 +38,7 @@ function ProjectForm({btnText}) {
             placeholder="Insira o orçamento total" 
             />
 
-            <Select name="Category_id" text="Selecione uma categoria"/>
+            <Select name="Category_id" text="Selecione uma categoria" options={categories}/>
 
             <Submit text={btnText}/>
 
