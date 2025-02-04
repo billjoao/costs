bcad46d9ad4a8c00d37a4eeb863b31cbbdd9c65e
João Eduardo Rocha Alves <joao.eduardo0418@hotmail.com>
2025-02-04 18:21:34 -0300

    alteraçao na pagina de projetos
    pequena alteraçao

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom"
 import Message from "../layout/Message"
+import styles from './Projects.module.css'
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 
 function Projects(){
@@ -10,9 +13,18 @@ function Projects(){
         message = location.state.message
     }
     return (
-        <div>
+        <div className={styles.project_container}>
+
+            <div className={styles.title_container}>
             <h1>Meus projetos</h1>
+            <LinkButton to="/newproject" text="Criar projeto!"/>
+            </div>
+
            {message &&  <Message msg={message} type="success"/>}
+
+           <Container>
+            <p>projetos...</p>
+           </Container>
         </div>
     )
 }
